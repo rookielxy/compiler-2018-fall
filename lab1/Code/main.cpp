a3e15fed574911d74c28a74684b55c6280ef2271
rookielxy <ryelovespython@gmail.com>
2018-09-27 22:26:02 +0800

    type 'make fake' to run C++

---
+++ lab1/Code/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include "ast.h"
+
+using namespace std;
+
+extern FILE* yyin;
+extern int yylval;
+int out;
+#ifdef YY_DEBUG
+extern int yydebug;
+#endif
+
+extern int yylex();
+extern int yyparse();
+extern int yyrestart(FILE *);
+
+
+int main(int argc , char **argv) {
+    out = 1;
+	if (argc <= 1)
+		return 1;
+	FILE *f = fopen(argv[1], "r");
+	if (f == NULL) {
+		perror(argv[1]);
+		return 1;
+	}
+
+	yyrestart(f);
+#ifdef YY_DEBUG
+	yydebug = 1;
+#endif
+	yyparse();
+    if (out == 1)
+	    travesalAst(astRoot, 0);
+	return 0;
+}
